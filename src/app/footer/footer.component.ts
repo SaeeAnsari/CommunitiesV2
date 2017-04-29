e11cd722126caefc705d2af0b497d8fd11309ccf
Saeed Ansari <saedansari@gmail.com>
2017-04-28 23:49:22 -0400

    commiting more changs befoe new post changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers: [UserService]
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _userService: UserService, private _router: Router) { }
 
   ngOnInit() {
   }
 
+  SetMainCityFeed() {
+    this._userService.getLoggedinInUser().subscribe(s => {
+      let defaultCommunityID = s.DefaultCommunityID;
+      this._router.navigate(['/Feed', defaultCommunityID]);
+    });
+  }
+
+  SetCommunitySearch() {
+    this._router.navigate(['/CommunitiesSearch']);
+  }
 }
--- src/app/live-feed/live-feed.component.ts
+++ src/app/live-feed/live-feed.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserPost } from '../interfaces/user-post';
 import { StoryService } from '../services/story.service';
 import { RouterModule, Router, ActivatedRoute } from '@angular/router';
-import {CommunityService} from '../services/community.service';
+import { CommunityService } from '../services/community.service';
 
 @Component({
   selector: 'app-live-feed',
@@ -17,52 +17,53 @@ export class LiveFeedComponent implements OnInit {
   private subscription;
   private communityID: number = 0;
   private pageIndex: number = 0;
-  private communityName:string = "";
+  private communityName: string = "";
 
-  constructor(private _storyService: StoryService, private _router: Router, private _route: ActivatedRoute, private _communityService:CommunityService) {
+  constructor(private _storyService: StoryService, private _router: Router, private _route: ActivatedRoute, private _communityService: CommunityService) {
     this.subscription = this._route.params.subscribe(params => {
       if (params["communityID"]) {
         this.communityID = +params["communityID"];
+        if (this.communityID > 0) {
+          this.getCommunityDetails();
+          this.loadStories();
+        }
       }
     });
   }
 
-  getCommunityDetails(){
+  getCommunityDetails() {
     this._communityService.GetCommunity(this.communityID)
-    .subscribe(sub=>{
-      this.communityName = sub.Name;
-    })
+      .subscribe(sub => {
+        this.communityName = sub.Name;
+      })
   }
 
-  loadStories(){
+  loadStories() {
+    this.posts = [];
     this._storyService.GetStoriesByCommunity(this.communityID, this.pageIndex)
-    .subscribe(postS => {
+      .subscribe(postS => {
 
-      postS.forEach(element => {
+        postS.forEach(element => {
 
-        this.posts.push({
-          storyID: element.ID,
-          title: element.Title,
-          text: element.LongDescription,
-          imageURL: element.ImageURL,
-          likeCount: element.ActionSummary.SupportCount,
-          dislikeCount: element.ActionSummary.DisagreeCount,
-          commentsCount: element.ActionSummary.CommentCount,
-          totalViews: element.ActionSummary.ViewCount,
-          userID: element.StoryUser.ID,
-          postDate: element.Timestamp,
-          userProfileImage: element.StoryUser.ImageURL,
-          userFullName: element.StoryUser.DisplayName
+          this.posts.push({
+            storyID: element.ID,
+            title: element.Title,
+            text: element.LongDescription,
+            imageURL: element.ImageURL,
+            likeCount: element.ActionSummary.SupportCount,
+            dislikeCount: element.ActionSummary.DisagreeCount,
+            commentsCount: element.ActionSummary.CommentCount,
+            totalViews: element.ActionSummary.ViewCount,
+            userID: element.StoryUser.ID,
+            postDate: element.Timestamp,
+            userProfileImage: element.StoryUser.ImageURL,
+            userFullName: element.StoryUser.DisplayName
+          });
         });
       });
-    });
   }
 
   ngOnInit() {
-    if(this.communityID > 0){
-      this.getCommunityDetails();
-    
-      this.loadStories();
-    }
+
   }
 }
